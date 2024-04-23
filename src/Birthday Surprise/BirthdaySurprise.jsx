ae94fad3bafa8e3c0fb0188fd6a5a4e84007e861
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-04-23 16:41:08 -0500

    reorder page elements in Birthday Surprise

--- src/Birthday Surprise/BirthdaySurprise.jsx
+++ src/Birthday Surprise/BirthdaySurprise.jsx
@@ -6,12 +6,13 @@ import { Spotify } from "react-spotify-embed";
 function BirthdaySurprise() {
   return (
     <div>
-      <Spotify wide link="https://open.spotify.com/playlist/2a3U47HXjEiaI5YA3LrUXf?si=50b7f21c3f144b1c" />
-      <img
+    <h1>Happy Birthday!</h1>
+    <img
         src="../assets/images/wish_bear_dancing_the_fortnite_floss_dance_emote_by_giromcalica_dctxi0u.gif"
         alt="Wish Bear dancing the 'Floss' dance from Fortnite."
       />
-      <h1>Happy Birthday!</h1>
+      <Spotify wide link="https://open.spotify.com/playlist/2a3U47HXjEiaI5YA3LrUXf?si=50b7f21c3f144b1c" />
+      
       {/* <ReactFloatingBalloons
         count={38}
         msgText="Happy Birthday!"
