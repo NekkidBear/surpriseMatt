c7abe4ba4de1a281cc4717c6dce157f26f8dd5f2
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-04-24 19:44:33 -0500

    add slide show for biscayne shoot

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import SurpriseButton from './Surprise Button/SurpriseButton';
 import BirthdaySurprise from './Birthday Surprise/BirthdaySurprise'
+import 'react-slideshow-image/dist/styles.css'
+import { Slide } from 'react-slideshow-image';
 
 function App() {
   const [showSurprise, setShowSurprise] = useState(false);
