6f5a5dd44dbf0e40b406733fc7beba7a7c8d94ce
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-04-25 17:38:06 -0500

    fix css for image

--- src/ToFromTag/ToFromTag.jsx
+++ src/ToFromTag/ToFromTag.jsx
@@ -1,13 +1,14 @@
 // ToFromTag.jsx
 import React from 'react';
 import groupScreenshot from '../assets/images/BiscayneGroupShot.jpg'; // replace with the actual path to your image
+import './ToFromTag.css';
 
 function ToFromTag() {
   return (
     <div className='toFromTag'>
       <h1 className='beginningH1'>To: Matt</h1>
       <br />
-      <img src={groupScreenshot} alt="Group screenshot" />
+      <img className='groupShot' src={groupScreenshot} alt="Group screenshot" />
       
     </div>
   );
